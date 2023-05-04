5edbf6cdae6cc9cc85551a130bc8ab4ad1df36db
Johnathan Aspinwall <jwaspin@gmail.com>
2023-05-04 12:34:36 -0600

    Add LocalizationProvider for dayjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import CssBaseline from '@mui/material/CssBaseline';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Provider } from 'react-redux';
 
 import AppContent from './components/AppContent';
@@ -7,10 +9,12 @@ import store from './store';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <CssBaseline />
-      <AppHeader />
-      <AppContent />
-    </Provider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Provider store={store}>
+        <CssBaseline />
+        <AppHeader />
+        <AppContent />
+      </Provider>
+    </LocalizationProvider>
   );
 }
