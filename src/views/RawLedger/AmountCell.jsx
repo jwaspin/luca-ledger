97e17d7845000d1bf76dfc738fbe61e992f4dfdb
Johnathan Aspinwall <jwaspin@gmail.com>
2023-05-04 08:56:32 -0600

    Develop edit and save amount

--- src/views/RawLedger/AmountCell.jsx
+++ src/views/RawLedger/AmountCell.jsx
@@ -1,5 +1,62 @@
-import { TableCell } from '@mui/material';
+import { Button, TableCell, TextField } from '@mui/material';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-export default function AmountCell({ amount }) {
-  return <TableCell>$ {parseFloat(amount).toFixed(2)}</TableCell>;
+import { updateTransaction } from '../../store/transactionsSlice';
+
+export default function AmountCell({ transaction }) {
+  const [edit, setEdit] = useState(false);
+  const [value, setValue] = useState(parseFloat(transaction.amount).toFixed(2));
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setValue(value);
+  };
+
+  const handleSave = () => {
+    const newTransaction = { ...transaction };
+    newTransaction.amount = parseFloat(parseFloat(value).toFixed(2));
+    setValue(newTransaction.amount);
+    dispatch(updateTransaction(newTransaction));
+    setEdit(false);
+  };
+
+  const handleCancel = () => {
+    setValue(parseFloat(transaction.amount).toFixed(2));
+    setEdit(false);
+  };
+
+  if (edit) {
+    return (
+      <TableCell>
+        <TextField
+          variant='filled'
+          value={value}
+          onChange={handleChange}
+        />
+        <Button
+          variant='contained'
+          onClick={handleSave}
+        >
+          Save
+        </Button>
+        <Button
+          variant='outlined'
+          onClick={handleCancel}
+        >
+          Cancel
+        </Button>
+      </TableCell>
+    );
+  }
+
+  return (
+    <TableCell
+      style={{ cursor: 'pointer' }}
+      onClick={() => setEdit(true)}
+    >
+      $ {parseFloat(transaction.amount).toFixed(2)}
+    </TableCell>
+  );
 }
