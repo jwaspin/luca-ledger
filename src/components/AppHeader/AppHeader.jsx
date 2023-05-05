44e2ee9a402289bb7bb0cd44c890e3c4516f7118
Johnathan Aspinwall <jwaspin@gmail.com>
2023-05-05 14:53:34 -0600

    Refactor to use MainMenu component

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react';
-
 import AppBar from '@mui/material/AppBar';
-import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-
-import MenuIcon from '@mui/icons-material/Menu';
+import MainMenu from './MainMenu';
 
 export default function AppHeader() {
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
     <AppBar position='static'>
       <Toolbar>
-        <IconButton
-          size='large'
-          edge='start'
-          color='inherit'
-          aria-label='menu'
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
+        <MainMenu />
         <Typography
           variant='h6'
           component='div'
