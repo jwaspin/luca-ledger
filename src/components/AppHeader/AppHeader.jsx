275f584cdca1687131b3304447643fb7240acdb8
Johnathan Aspinwall <jwaspin@gmail.com>
2023-05-07 12:14:54 -0600

    Cleanup layout

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,9 +1,9 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 
 import { useSelector } from 'react-redux';
 import { selectTransactions } from '../../store/transactionsSlice';
-import MainMenu from './MainMenu';
 import AccountNameField from './AccountNameField';
+import MainMenu from './MainMenu';
 
 export default function AppHeader() {
   const transactions = useSelector(selectTransactions);
@@ -11,17 +11,15 @@ export default function AppHeader() {
 
   return (
     <AppBar position='static'>
-      <Toolbar>
-        <MainMenu />
-        <Typography
-          variant='h4'
-          component='div'
-          sx={{ flexGrow: 1 }}
-        >
-          Finance Tracker
-        </Typography>
-        <AccountNameField accountName={accountName} />
-        {modified && <Typography variant='body1'>Modified</Typography>}
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Box sx={{ flexDirection: 'row', display: 'flex' }}>
+          <MainMenu />
+          <Typography variant='h4'>Finance Tracker</Typography>
+        </Box>
+        <Box sx={{ flexDirection: 'row', display: 'flex' }}>
+          <AccountNameField accountName={accountName} />
+          {modified && <Typography variant='body1'>Modified</Typography>}
+        </Box>
       </Toolbar>
     </AppBar>
   );
