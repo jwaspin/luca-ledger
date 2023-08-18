eb9de7b3e0fde02e01e74c1ba398dc7dfd796c5f
Johnathan Aspinwall <jwaspin@gmail.com>
2023-08-18 17:20:20 -0600

    Styled Header

--- src/components/MainLayout/AppHeader/AppHeader.jsx
+++ src/components/MainLayout/AppHeader/AppHeader.jsx
@@ -1,10 +1,23 @@
+import styled from '@emotion/styled';
 import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 
 import { selectAccountById } from '@/store/accountsSlice';
 import AccountNameField from './AccountNameField';
-import MainMenu from './MainMenu';
+
+import HomeIcon from '@mui/icons-material/Home';
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  color: white;
+  &:hover: {
+    text-decoration: underline;
+  }
+`;
 
 export default function AppHeader() {
   const { accountId } = useParams();
@@ -18,13 +31,39 @@ export default function AppHeader() {
 
   return (
     <AppBar position='static'>
-      <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <Box sx={{ flexDirection: 'row', display: 'flex' }}>
-          <MainMenu />
-          <Link to='/'>Dashboard</Link>
-          <Link to='/accounts'>Accounts</Link>
-          <Typography variant='h4'>Finance Tracker</Typography>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Link to='/'>
+            <HomeIcon
+              sx={{
+                fontSize: '3rem',
+                color: 'white',
+                textDecoration: 'none',
+                '&:hover': { textDecoration: 'underline' },
+              }}
+            />
+          </Link>
+          <StyledLink to='/accounts'>
+            <Typography
+              variant='body2'
+              style={{
+                display: 'block',
+                marginLeft: '50px',
+                fontSize: '1.5rem',
+                fontWeight: 'bold',
+                color: 'white',
+              }}
+            >
+              Accounts
+            </Typography>
+          </StyledLink>
         </Box>
+        <Typography
+          variant='h4'
+          sx={{ flexGrow: 1, textAlign: 'center' }}
+        >
+          Finance Tracker
+        </Typography>
         {accountInfo}
       </Toolbar>
     </AppBar>
