6772f872c52eb44e84c3ed2ae8f5719da9e257c2
Johnathan Aspinwall <jwaspin@gmail.com>
2023-08-19 09:22:59 -0600

    Refactor with Accounts Slice

--- src/views/AccountLedger/AmountCell.jsx
+++ src/views/AccountLedger/AmountCell.jsx
@@ -1,29 +1,39 @@
 import { Button, TableCell, TextField } from '@mui/material';
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-import { updateTransaction } from '../../store/transactionsSlice';
+import { selectAccounts } from '@/store/accountsSlice';
+import { updateTransaction } from '@/store/transactionsSlice';
 
 const parseFloatDoublePrecision = (value) =>
   parseFloat(parseFloat(value).toFixed(2));
 
 export default function AmountCell({ transaction }) {
+  const dispatch = useDispatch();
+  const { accountId } = useParams();
+  const accounts = useSelector(selectAccounts);
   const [edit, setEdit] = useState(false);
   const [value, setValue] = useState(
     parseFloatDoublePrecision(transaction.amount)
   );
-  const dispatch = useDispatch();
 
   const handleChange = (event) => {
     const { value } = event.target;
     setValue(value);
   };
 
-  const handleSave = () => {
-    const newTransaction = { ...transaction };
-    newTransaction.amount = parseFloatDoublePrecision(value);
-    setValue(newTransaction.amount);
-    dispatch(updateTransaction(newTransaction));
+  const handleUpdateTransaction = (accountId, transaction) => {
+    const account = accounts.find((a) => a.id === accountId);
+    if (account) {
+      const updatedTransaction = { ...transaction, modified: true };
+      dispatch(
+        updateTransaction({
+          accountId,
+          transaction: updatedTransaction,
+        })
+      );
+    }
     setEdit(false);
   };
 
@@ -42,7 +52,7 @@ export default function AmountCell({ transaction }) {
         />
         <Button
           variant='contained'
-          onClick={handleSave}
+          onClick={() => handleUpdateTransaction(accountId, transaction)}
         >
           Save
         </Button>
