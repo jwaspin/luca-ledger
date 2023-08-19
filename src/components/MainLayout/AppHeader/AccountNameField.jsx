d07a0532731dc30d97970dd1da0e37479e9aac40
Johnathan Aspinwall <jwaspin@gmail.com>
2023-08-19 09:20:59 -0600

    Refactoring with Accounts Slice

--- src/components/MainLayout/AppHeader/AccountNameField.jsx
+++ src/components/MainLayout/AppHeader/AccountNameField.jsx
@@ -1,13 +1,16 @@
 import { TextField } from '@mui/material';
 import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-import { modifyAccountName } from '@/store/transactionsSlice';
+import { updateAccount } from '@/store/accountsSlice';
 
 export default function AccountNameField({ accountName }) {
   const dispatch = useDispatch();
+  const { accountId } = useParams();
 
   const handleChange = (event) => {
-    dispatch(modifyAccountName(event.target.value));
+    const { value } = event.target;
+    dispatch(updateAccount({ id: accountId, name: value }));
   };
 
   return (
