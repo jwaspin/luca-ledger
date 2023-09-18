afa2627f79c6d444c0602a57e016167c1897e6a0
Johnathan Aspinwall <jwaspin@gmail.com>
2023-09-17 18:49:51 -0600

    New Repeated Transactions Modal
    New index file
    Updates to the transactions slice.

--- src/store/transactionsSlice.js
+++ src/store/transactionsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import dayjs from 'dayjs';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -46,11 +46,52 @@ const generateTransactionObject = (id, status, date, amount, description) => ({
   description,
 });
 
-export const createNewTransaction = () =>
+export const createNewTransaction = (date = null) =>
   generateTransactionObject(
     uuidv4(),
     TransactionStatusEnum.PLANNED,
-    dayjs().format(config.dateFormatString),
+    date || dayjs().format(config.dateFormatString),
     0.0,
     ''
   );
+
+export const createRepeatTransaction = createAsyncThunk(
+  'transactions/createRepeat',
+  async (
+    {
+      startDate,
+      amount,
+      description,
+      frequency,
+      frequencyCount,
+      occurrences,
+      accountId,
+    },
+    { dispatch }
+  ) => {
+    let nextDate = dayjs(startDate);
+
+    for (let i = 0; i < occurrences; i++) {
+      const newTransaction = createNewTransaction(
+        nextDate.format(config.dateFormatString)
+      );
+      newTransaction.amount = amount;
+      newTransaction.description = description;
+
+      const actionPayload = { accountId, transaction: newTransaction };
+      dispatch(addTransaction(actionPayload));
+
+      if (frequency === 'Days') {
+        nextDate = nextDate.add(frequencyCount, 'day');
+      } else if (frequency === 'Weeks') {
+        nextDate = nextDate.add(frequencyCount, 'week');
+      } else if (frequency === 'Months') {
+        nextDate = nextDate.add(frequencyCount, 'month');
+      } else if (frequency === 'Years') {
+        nextDate = nextDate.add(frequencyCount, 'year');
+      } else if (frequency === 'Bi-Monthly') {
+        nextDate = nextDate.add(frequencyCount * 2, 'month');
+      }
+    }
+  }
+);
