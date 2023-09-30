041c63f2f889591ed2be4a0963ea627c7684a950
Johnathan Aspinwall <jwaspin@gmail.com>
2023-09-30 08:22:28 -0600

    Add Red Background for Negative Balance

--- src/views/AccountLedger/BalanceCell.jsx
+++ src/views/AccountLedger/BalanceCell.jsx
@@ -2,7 +2,13 @@ import { TableCell } from '@mui/material';
 import PropTypes from 'prop-types';
 
 export default function BalanceCell({ amount }) {
-  return <TableCell>$ {parseFloat(amount).toFixed(2)}</TableCell>;
+  const cellStyle = {
+    backgroundColor: amount < 0 ? '#CC4040' : 'inherit',
+  };
+
+  return (
+    <TableCell style={cellStyle}>$ {parseFloat(amount).toFixed(2)}</TableCell>
+  );
 }
 
 BalanceCell.propTypes = {
