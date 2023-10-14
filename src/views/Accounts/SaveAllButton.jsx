6c11a642eaf162a9447332119d827563e55040e2
Johnathan Aspinwall <jwaspin@gmail.com>
2023-10-14 13:16:49 -0600

    Import Schemas
    Account schemas are now imported and
    the version is added to the account when
    loaded or created. Version is now exported
    as part of the account data.

--- src/views/Accounts/SaveAllButton.jsx
+++ src/views/Accounts/SaveAllButton.jsx
@@ -1,21 +1,10 @@
 import { Button } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-import { selectAccounts } from '@/store/accountsSlice';
+import { selectAccounts, saveAccount } from '@/store/accountsSlice';
 
 export default function SaveAllButton() {
   const accounts = useSelector(selectAccounts);
-
-  const saveAccount = (account, filename) => {
-    const saveString = JSON.stringify(account, null, 2);
-    const saveBlob = new Blob([saveString]);
-    const url = URL.createObjectURL(saveBlob);
-    const link = document.createElement('a');
-    link.download = filename;
-    link.href = url;
-    link.click();
-    URL.revokeObjectURL(url);
-  };
 
   const handleSaveAllAccounts = () => {
     accounts.forEach((account) => {
