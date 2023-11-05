dba7723affb69cbdbbbd3809470a18735c104cbc
Johnathan <jwaspin@users.noreply.github.com>
2023-11-05 09:34:54 -0700

    Clean up style (#90)
    * Improvements
    
    Cleaned up style.
    Enter to save, escape to cancel.
    Focus on click.
    Clear input if default on click.
    
    * New Regex Validation
    
    * Remove Console Logs
    
    * Clean Up
    
    Improved UX with date picker.
    Improved style when edit enabled.
    
    * Improve UX
    
    Using timeouts to control auto-save when typing vs mouse clicks.

--- src/components/LedgerTable/AmountCell.jsx
+++ src/components/LedgerTable/AmountCell.jsx
@@ -1,6 +1,12 @@
-import { Button, InputAdornment, TableCell, TextField } from '@mui/material';
+import {
+  Button,
+  InputAdornment,
+  TableCell,
+  TextField,
+  Typography,
+} from '@mui/material';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
@@ -10,29 +16,49 @@ import {
   parseFloatDoublePrecision,
 } from '@/utils';
 
+import { Cancel, Check } from '@mui/icons-material';
+
 export default function AmountCell({ transaction }) {
   const dispatch = useDispatch();
   const { accountId } = useParams();
+  const inputRef = useRef(null);
   const [edit, setEdit] = useState(false);
   const [value, setValue] = useState(
     parseFloatDoublePrecision(transaction.amount)
   );
 
+  const validNumberRegex = /^-?\d+(\.\d{1,2})?$|^-?\.\d{1,2}$|^-?\d+\.$|^-?$/;
+
   const handleChange = (event) => {
     const { value } = event.target;
-    setValue(parseFloatDoublePrecision(value));
+    if (value === '-') {
+      setValue(value);
+    } else if (value === '') {
+      setValue(value);
+    } else if (validNumberRegex.test(value)) {
+      setValue(value);
+    }
   };
 
   const handleSave = () => {
-    dispatch(
-      actions.updateTransactionProperty(
-        accountId,
-        transaction,
-        constants.TransactionFields.AMOUNT,
-        value
-      )
-    );
-    setEdit(false);
+    let newValue = value;
+    if (newValue === '') {
+      newValue = 0;
+    } else if (newValue === '-') {
+      newValue = 0;
+    }
+    if (validNumberRegex.test(newValue)) {
+      newValue = parseFloatDoublePrecision(newValue);
+      dispatch(
+        actions.updateTransactionProperty(
+          accountId,
+          transaction,
+          constants.TransactionFields.AMOUNT,
+          newValue
+        )
+      );
+      setEdit(false);
+    }
   };
 
   const handleCancel = () => {
@@ -40,41 +66,65 @@ export default function AmountCell({ transaction }) {
     setEdit(false);
   };
 
-  if (edit) {
-    return (
-      <TableCell>
-        <TextField
-          variant='filled'
-          type='number'
-          value={value}
-          onChange={handleChange}
-          inputProps={{ step: '0.01' }}
-          InputProps={{
-            startAdornment: <InputAdornment position='start'>$</InputAdornment>,
-          }}
-        />
-        <Button
-          variant='contained'
-          onClick={handleSave}
-        >
-          Save
-        </Button>
-        <Button
-          variant='outlined'
-          onClick={handleCancel}
-        >
-          Cancel
-        </Button>
-      </TableCell>
-    );
-  }
+  const handleEdit = () => {
+    if (value === 0) setValue('');
+    setEdit(true);
+    setTimeout(() => {
+      inputRef.current.focus();
+    }, 0);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSave();
+    } else if (event.key === 'Escape') {
+      handleCancel();
+    }
+  };
 
   return (
-    <TableCell
-      style={{ cursor: 'pointer', width: '150px' }}
-      onClick={() => setEdit(true)}
-    >
-      $ {doublePrecisionFormatString(transaction.amount)}
+    <TableCell style={{ cursor: 'pointer', width: '200px' }}>
+      {edit ? (
+        <>
+          <TextField
+            variant='filled'
+            type='text'
+            value={value}
+            inputRef={inputRef}
+            onChange={handleChange}
+            onKeyDown={handleKeyPress}
+            inputProps={{ step: '0.01' }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>$</InputAdornment>
+              ),
+            }}
+          />
+          <Button
+            variant='contained'
+            width='100px'
+            height='30px'
+            onClick={handleSave}
+          >
+            <Check />
+          </Button>
+          <Button
+            variant='outlined'
+            width='100px'
+            height='30px'
+            onClick={handleCancel}
+          >
+            <Cancel />
+          </Button>
+        </>
+      ) : (
+        <Typography
+          variant='body1'
+          onClick={handleEdit}
+        >
+          $ {doublePrecisionFormatString(transaction.amount)}
+        </Typography>
+      )}
     </TableCell>
   );
 }
