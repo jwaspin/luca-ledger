a77818cc11b14381d9322d336ae86cd748e54e4b
Johnathan Aspinwall <jwaspin@gmail.com>
2023-11-04 21:05:25 -0600

    Improvements
    Cleaned up style.
    Enter to save, escape to cancel.
    Focus on click.
    Clear input if default on click.

--- src/components/LedgerTable/AmountCell.jsx
+++ src/components/LedgerTable/AmountCell.jsx
@@ -1,6 +1,12 @@
-import { Button, InputAdornment, TableCell, TextField } from '@mui/material';
+import {
+  Button,
+  InputAdornment,
+  TableCell,
+  TextField,
+  Typography,
+} from '@mui/material';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
@@ -10,9 +16,12 @@ import {
   parseFloatDoublePrecision,
 } from '@/utils';
 
+import { Cancel, Check } from '@mui/icons-material';
+
 export default function AmountCell({ transaction }) {
   const dispatch = useDispatch();
   const { accountId } = useParams();
+  const inputRef = useRef(null);
   const [edit, setEdit] = useState(false);
   const [value, setValue] = useState(
     parseFloatDoublePrecision(transaction.amount)
@@ -24,6 +33,7 @@ export default function AmountCell({ transaction }) {
   };
 
   const handleSave = () => {
+    console.log('handleSave');
     dispatch(
       actions.updateTransactionProperty(
         accountId,
@@ -40,41 +50,65 @@ export default function AmountCell({ transaction }) {
     setEdit(false);
   };
 
-  if (edit) {
-    return (
-      <TableCell>
-        <TextField
-          variant='filled'
-          type='number'
-          value={value}
-          onChange={handleChange}
-          inputProps={{ step: '0.01' }}
-          InputProps={{
-            startAdornment: <InputAdornment position='start'>$</InputAdornment>,
-          }}
-        />
-        <Button
-          variant='contained'
-          onClick={handleSave}
-        >
-          Save
-        </Button>
-        <Button
-          variant='outlined'
-          onClick={handleCancel}
-        >
-          Cancel
-        </Button>
-      </TableCell>
-    );
-  }
+  const handleEdit = () => {
+    if (value === 0) setValue('');
+    setEdit(true);
+    setTimeout(() => {
+      inputRef.current.focus();
+    }, 0);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSave();
+    } else if (event.key === 'Escape') {
+      handleCancel();
+    }
+  };
 
   return (
-    <TableCell
-      style={{ cursor: 'pointer', width: '150px' }}
-      onClick={() => setEdit(true)}
-    >
-      $ {doublePrecisionFormatString(transaction.amount)}
+    <TableCell style={{ cursor: 'pointer', width: '200px' }}>
+      {edit ? (
+        <>
+          <TextField
+            variant='filled'
+            type='number'
+            value={value}
+            inputRef={inputRef}
+            onChange={handleChange}
+            onKeyDown={handleKeyPress}
+            inputProps={{ step: '0.01' }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>$</InputAdornment>
+              ),
+            }}
+          />
+          <Button
+            variant='contained'
+            width='100px'
+            height='30px'
+            onClick={handleSave}
+          >
+            <Check />
+          </Button>
+          <Button
+            variant='outlined'
+            width='100px'
+            height='30px'
+            onClick={handleCancel}
+          >
+            <Cancel />
+          </Button>
+        </>
+      ) : (
+        <Typography
+          variant='body1'
+          onClick={handleEdit}
+        >
+          $ {doublePrecisionFormatString(transaction.amount)}
+        </Typography>
+      )}
     </TableCell>
   );
 }
