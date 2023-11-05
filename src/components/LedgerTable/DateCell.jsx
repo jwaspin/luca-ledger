52c52e77dc2d9bef22750a6ada6fe7d35791c754
Johnathan Aspinwall <jwaspin@gmail.com>
2023-11-05 08:47:04 -0700

    Clean Up
    Improved UX with date picker.
    Improved style when edit enabled.

--- src/components/LedgerTable/DateCell.jsx
+++ src/components/LedgerTable/DateCell.jsx
@@ -1,4 +1,4 @@
-import { Button, TableCell } from '@mui/material';
+import { Box, Button, TableCell, Typography } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
 import PropTypes from 'prop-types';
@@ -8,24 +8,27 @@ import { useParams } from 'react-router-dom';
 
 import config from '@/config';
 import { actions, constants } from '@/store/transactions';
+import { Cancel, Check } from '@mui/icons-material';
 
 export default function DateCell({ transaction }) {
   const dispatch = useDispatch();
   const { accountId } = useParams();
   const [edit, setEdit] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [dateValue, setDateValue] = useState(
     dayjs(transaction.date, config.dateFormatString)
   );
 
-  const handleSave = () => {
+  const handleSave = (value) => {
     dispatch(
       actions.updateTransactionProperty(
         accountId,
         transaction,
         constants.TransactionFields.DATE,
-        dateValue.format(config.dateFormatString)
+        value.format(config.dateFormatString)
       )
     );
+    setIsOpen(false);
     setEdit(false);
   };
 
@@ -34,38 +37,56 @@ export default function DateCell({ transaction }) {
     setEdit(false);
   };
 
-  if (edit) {
-    return (
-      <TableCell>
-        <DatePicker
-          value={dateValue}
-          onChange={(newValue) => setDateValue(newValue)}
-        />
+  const handleEdit = () => {
+    setEdit(true);
+    setIsOpen(true);
+  };
+
+  const handleChange = (value) => {
+    setDateValue(value);
+    handleSave(value);
+  };
+
+  const buttonStyle = {
+    height: '30px',
+    width: '75px',
+  };
+
+  const editComponent = (
+    <Box style={{ display: 'flex', flexDirection: 'column' }}>
+      <DatePicker
+        value={dateValue}
+        onChange={handleChange}
+        open={isOpen}
+      />
+      <Box style={{ display: 'flex', flexDirection: 'row' }}>
         <Button
           variant='contained'
-          onClick={handleSave}
+          style={buttonStyle}
+          onClick={() => handleSave(dateValue)}
         >
-          Save
+          <Check />
         </Button>
         <Button
           variant='outlined'
+          style={buttonStyle}
           onClick={handleCancel}
         >
-          Cancel
+          <Cancel />
         </Button>
-      </TableCell>
-    );
-  }
+      </Box>
+    </Box>
+  );
 
   return (
-    <TableCell
-      style={{
-        cursor: 'pointer',
-        width: '150px',
-      }}
-      onClick={() => setEdit(true)}
-    >
-      {dateValue.format('MMM DD YYYY')}
+    <TableCell style={{ cursor: 'pointer', width: '160px' }}>
+      {edit ? (
+        editComponent
+      ) : (
+        <Typography onClick={handleEdit}>
+          {dateValue.format('MMM DD YYYY')}
+        </Typography>
+      )}
     </TableCell>
   );
 }
