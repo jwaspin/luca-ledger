294d2dfd6a04bda1f318f481bb241197ed891e0f
Johnathan Aspinwall <jwaspin@gmail.com>
2023-11-07 16:10:24 -0700

    New Home Content

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,30 @@
+import { Box, Typography } from '@mui/material';
+
+import config from '@/config';
+
 export default function Home() {
-    return (
-        <div>
-        <h1>Home</h1>
-        </div>
-    );
+  return (
+    <Box
+      style={{
+        borderBottom: '3px solid black',
+        padding: '50px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Typography
+        variant='h2'
+        style={{ width: '100%', textAlign: 'center', marginBottom: '50px' }}
+      >
+        Welcome to Luca Ledger
+      </Typography>
+      <Typography
+        variant='body1'
+        style={{ width: '75%', textAlign: 'justify' }}
+      >
+        {config.homeText}
+      </Typography>
+    </Box>
+  );
 }
