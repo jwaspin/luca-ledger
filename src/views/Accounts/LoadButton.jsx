1b864ce9fc2a35f7762168e163b10249100977a4
Johnathan Aspinwall <jwaspin@gmail.com>
2023-11-07 21:07:15 -0700

    Refactor Load Account Button
    Now using the <input> tag so that it's compatible with multiple browsers

--- src/views/Accounts/LoadButton.jsx
+++ src/views/Accounts/LoadButton.jsx
@@ -1,18 +1,44 @@
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
+import { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { actions } from '@/store/accounts';
 
 export default function LoadButton() {
   const dispatch = useDispatch();
+  const fileInputRef = useRef(null);
+
+  const handleChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      const json = JSON.parse(reader.result);
+      dispatch(actions.loadAccount(json));
+    };
+    reader.readAsText(file);
+  };
+
+  const handleLoadAccountsClick = () => {
+    fileInputRef.current.click();
+  };
 
   return (
-    <Button
-      variant='contained'
-      color='primary'
-      onClick={() => dispatch(actions.loadAccountAsync())}
-    >
-      Load Account
-    </Button>
+    <Box>
+      <Button
+        variant='contained'
+        color='primary'
+        onClick={handleLoadAccountsClick}
+      >
+        Load Accounts
+      </Button>
+      <input
+        type='file'
+        id='fileInput'
+        ref={fileInputRef}
+        onChange={handleChange}
+        style={{ display: 'none' }}
+        multiple
+      />
+    </Box>
   );
 }
