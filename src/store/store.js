aa9b67ab20fe94d49eceb03f4544114f731135a8
Johnathan Aspinwall <jwaspin@gmail.com>
2023-11-09 09:54:45 -0700

    New Local Storage Middleware

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import rootReducer from './rootReducer';
 
+const localStorageMiddleware = (store) => (next) => (action) => {
+  const result = next(action);
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+  return result;
+};
+
 export default configureStore({
   reducer: rootReducer,
+  preloadedState: JSON.parse(localStorage.getItem('reduxState')) || {},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
 });
