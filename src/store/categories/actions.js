4a79101e1b5829338d72a0256f8b13c856642296
Johnathan Aspinwall <jwaspin@gmail.com>
2023-11-12 15:36:06 -0700

    Remove and Update Actions

--- src/store/categories/actions.js
+++ src/store/categories/actions.js
@@ -1,5 +1,5 @@
 import { generateCategory } from './generators';
-import { addCategory } from './slice';
+import { addCategory, removeCategory, updateCategory } from './slice';
 
 export const createNewCategory = (initialData) => (dispatch) => {
   dispatch(addCategory(generateCategory(initialData)));
@@ -7,4 +7,12 @@ export const createNewCategory = (initialData) => (dispatch) => {
 
 export const loadCategory = (category) => (dispatch) => {
   dispatch(addCategory(generateCategory(category)));
+};
+
+export const removeCategoryById = (id) => (dispatch) => {
+  dispatch(removeCategory(id));
+};
+
+export const updateCategoryName = (id, name) => (dispatch) => {
+  dispatch(updateCategory({ id, name }));
 };
