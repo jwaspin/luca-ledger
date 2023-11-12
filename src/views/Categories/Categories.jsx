a413a083193b5dcf85ea23d942bad82c726c758a
Johnathan Aspinwall <jwaspin@gmail.com>
2023-11-12 15:03:14 -0700

    List and Add Categories

--- src/views/Categories/Categories.jsx
+++ src/views/Categories/Categories.jsx
@@ -1,7 +1,46 @@
+import { Box, Button } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+
+import CreateNewCategory from '@/components/CreateNewCategory';
+import { actions, constants, selectors } from '@/store/categories';
+
 export default function Categories() {
+  const dispatch = useDispatch();
+  const categories = useSelector(selectors.selectCategories);
+
+  const loadDefaultCategories = () => {
+    constants.CategoryInitialData.forEach((category) => {
+      dispatch(actions.loadCategory(category));
+    });
+  };
+
   return (
-    <div>
-      <h1>Categories</h1>
-    </div>
+    <Box
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '1rem',
+      }}
+    >
+      <Box>Categories</Box>
+      <Button onClick={loadDefaultCategories}>Load Default Categories</Button>
+      <CreateNewCategory />
+      {categories.map((category) => (
+        <Box
+          key={category.id}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: '5px',
+            width: '250px',
+            border: '1px solid black',
+          }}
+        >
+          {category.name}
+        </Box>
+      ))}
+    </Box>
   );
 }
