d684b687d168ca847fbae5a88c7c4b52977443e7
Johnathan <jwaspin@users.noreply.github.com>
2024-03-14 07:21:52 -0600

    Sync main/dev (#161)
    * Add New User Guide (#141) (#142)
    
    * User Notice about v2.0 (#159)
    
    * Refactor README.md to include notice about upcoming version 2.0
    
    * 1.3.3
    
    * Add COMING SOON message for Version 2.0
    
    * Update Home.jsx with Version 2.0 details and new domain discussion

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -20,6 +20,11 @@ export default function Home() {
         >
           Welcome to Luca Ledger
         </Typography>
+        <Typography>
+          COMING SOON!!! Version 2.0! Stay tuned for updates!
+          <p>Version 2 will see a major update to the schemas for how the data is being stored. This will allow more front end features to be developed making this application even more powerful!</p>
+          <p><em>New Domain!</em> Version 2 will also probably see changes to the domain, but, if anyone is actually using this application, I am interested in your feedback regarding the name and domain of this application. I am planning on making this into a more official open source project hopefully to attract more interest and user support. I have created <a href="https://github.com/jwaspin/luca-ledger/issues/160">this issue on GitHub</a> to try and capture some discussion around this topic. Please contribute if you're interested! It would be good to know if I actually have any active users for this application yet, especially to communicate these coming changes more effectively.</p>
+        </Typography>
         <Typography
           variant='body1'
           style={{ width: '75%', textAlign: 'justify', marginBottom: '50px' }}
