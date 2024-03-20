49780cfd61373ec50edfa8123aa1afd260e71e34
Johnathan Aspinwall <jwaspin@gmail.com>
2024-03-20 15:04:04 -0600

    Update entities.json with new entity data and add entities.test.js

---
+++ schema/v2.0/tests/entities.test.js
@@ -0,0 +1,16 @@
+const Ajv2020 = require("ajv/dist/2020");
+const addFormats = require("ajv-formats");
+const schema = require("../entity.json");
+const exampleData = require("../examples/entities.json");
+
+const ajv = new Ajv2020();
+addFormats(ajv);
+const validate = ajv.compile(schema);
+
+test('examples are valid entities', () => {
+    exampleData.forEach((example) => {
+        const valid = validate(example);
+        expect(valid).toBe(true);
+        if (!valid) console.log(validate.errors);
+    });
+});
