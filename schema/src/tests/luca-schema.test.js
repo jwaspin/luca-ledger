2a9f71a9717fc30c03f56af6c01bf6f01cc2574e
Johnathan Aspinwall <jwaspin@gmail.com>
2024-03-31 13:39:41 -0600

    Refactor schema file paths and update imports

--- schema/src/tests/luca-schema.test.js
+++ schema/src/tests/luca-schema.test.js
@@ -1,9 +1,20 @@
 const Ajv2020 = require("ajv/dist/2020");
 const addFormats = require("ajv-formats");
+const axios = require("axios");
+
 const schema = require("../luca-schema.json");
 const exampleData = require("../examples/luca-schema.json");
 
-const ajv = new Ajv2020();
+async function loadSchema(uri) {
+  try {
+    const response = await axios.get(uri);
+    return response.data;
+  } catch (error) {
+    console.error(`Could not load schema from ${uri}:`, error);
+  }
+}
+
+const ajv = new Ajv2020({ loadSchema });
 addFormats(ajv);
 const validate = ajv.compile(schema);
 
