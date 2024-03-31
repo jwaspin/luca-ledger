fea004b19ff92cab1be63e96dda6be79481774d4
Johnathan Aspinwall <jwaspin@gmail.com>
2024-03-31 14:50:57 -0600

    Refactor schema file paths and update imports for schema files

--- schema/index.js
+++ schema/index.js
@@ -2,6 +2,7 @@ const categorySchema = require('./src/schemas/category.json');
 const entitySchema = require('./src/schemas/entity.json');
 const recurringTransactionSchema = require('./src/schemas/recurringTransaction.json');
 const recurringTransactionEventSchema = require('./src/schemas/recurringTransactionEvent.json');
+const schemaSchema = require('./src/schemas/schema.json');
 const transactionSchema = require('./src/schemas/transaction.json');
 const lucaSchema = require('./src/luca-schema.json');
 
@@ -11,5 +12,6 @@ module.exports = {
   entitySchema,
   recurringTransactionSchema,
   recurringTransactionEventSchema,
+  schemaSchema,
   transactionSchema,
 };
