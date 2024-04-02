6529b1d1098cd94e6c0315f62a78109a46911e08
Johnathan Aspinwall <jwaspin@gmail.com>
2024-04-01 22:08:02 -0600

    Refactor ImportPage component and add LoadButton component

--- client/src/features/import/ImportPage.jsx
+++ client/src/features/import/ImportPage.jsx
@@ -1,40 +1,10 @@
-// import { useState } from 'react';
+import LoadButton from './components/LoadButton';
 
 export default function ImportPage() {
-  //   const [importData, setImportData] = useState(null);
-  //   const [importStatus, setImportStatus] = useState(null);
-
-  //   const handleFileChange = (e) => {
-  //     const file = e.target.files[0];
-  //     const reader = new FileReader();
-  //     reader.onload = (e) => {
-  //       setImportData(e.target.result);
-  //     };
-  //     reader.readAsText(file);
-  //   };
-
-  //   const handleImport = async () => {
-  //     console.log('Importing...');
-  //     // try {
-  //     //   setImportStatus('loading');
-  //     //   await importTransactions(importData);
-  //     //   setImportStatus('success');
-  //     // } catch (error) {
-  //     //   setImportStatus('error');
-  //     // }
-  //   };
-
   return (
     <div>
       <h1>Import Transactions</h1>
-      {/* <input
-        type='file'
-        onChange={handleFileChange}
-      />
-      <button onClick={handleImport}>Import</button>
-      {importStatus === 'loading' && <p>Loading...</p>}
-      {importStatus === 'success' && <p>Import successful</p>}
-      {importStatus === 'error' && <p>Import failed</p>} */}
+      <LoadButton />
     </div>
   );
 }
