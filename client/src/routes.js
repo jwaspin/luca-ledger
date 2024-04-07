a5b455540409bc45a61598c4ffb925f5159e81db
Johnathan Aspinwall <jwaspin@gmail.com>
2024-04-06 18:18:32 -0600

    Refactor NavItems component to disable unused items

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,13 +1,13 @@
 import HomePage from '@/features/home/HomePage';
 // import DashboardPage from '@/features/dashboard/DashboardPage';
-import AccountsPage from '@/features/accounts/AccountsPage';
-import LedgerPage from '@/features/ledger/LedgerPage';
-import EntitiesPage from '@/features/entities/EntitiesPage';
-import CategoriesPage from '@/features/categories/CategoriesPage';
-import RecurringTransactionsPage from '@/features/recurringTransactions/RecurringTransactionsPage';
+// import AccountsPage from '@/features/accounts/AccountsPage';
+// import LedgerPage from '@/features/ledger/LedgerPage';
+// import EntitiesPage from '@/features/entities/EntitiesPage';
+// import CategoriesPage from '@/features/categories/CategoriesPage';
+// import RecurringTransactionsPage from '@/features/recurringTransactions/RecurringTransactionsPage';
 // import BusinessesPage from '@/features/businesses/BusinessesPage';
 // import SettingsPage from '@/features/settings/SettingsPage';
-import ExportPage from '@/features/export/ExportPage';
+// import ExportPage from '@/features/export/ExportPage';
 import ImportPage from '@/features/import/ImportPage';
 import NotFoundPage from '@/features/errors/NotFoundPage';
 
@@ -20,26 +20,26 @@ export const routes = [
   //   path: '/dashboard',
   //   component: DashboardPage,
   // },
-  {
-    path: '/accounts',
-    component: AccountsPage,
-  },
-  {
-    path: '/accounts/:accountId',
-    component: LedgerPage,
-  },
-  {
-    path: '/entities',
-    component: EntitiesPage,
-  },
-  {
-    path: '/categories',
-    component: CategoriesPage,
-  },
-  {
-    path: '/recurring',
-    component: RecurringTransactionsPage,
-  },
+  // {
+  //   path: '/accounts',
+  //   component: AccountsPage,
+  // },
+  // {
+  //   path: '/accounts/:accountId',
+  //   component: LedgerPage,
+  // },
+  // {
+  //   path: '/entities',
+  //   component: EntitiesPage,
+  // },
+  // {
+  //   path: '/categories',
+  //   component: CategoriesPage,
+  // },
+  // {
+  //   path: '/recurring',
+  //   component: RecurringTransactionsPage,
+  // },
   // {
   //   path: '/businesses',
   //   component: BusinessesPage,
@@ -48,10 +48,10 @@ export const routes = [
   //   path: '/settings',
   //   component: SettingsPage,
   // },
-  {
-    path: '/export',
-    component: ExportPage,
-  },
+  // {
+  //   path: '/export',
+  //   component: ExportPage,
+  // },
   {
     path: '/import',
     component: ImportPage,
