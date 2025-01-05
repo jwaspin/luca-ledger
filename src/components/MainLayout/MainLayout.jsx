8dd565cedb70834d2d9f99315ee63d8458ef74c2
Johnathan Aspinwall <jwaspin@gmail.com>
2025-01-05 10:50:38 -0700

    Refactor MainLayout to use flexbox for improved layout structure

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -4,10 +4,12 @@ import AppHeader from './AppHeader';
 
 export default function MainLayout() {
   return (
-    <>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
       <AppHeader />
-      <AppContent />
+      <div style={{ flex: 1 }}>
+        <AppContent />
+      </div>
       <AppFooter />
-    </>
+    </div>
   );
 }
