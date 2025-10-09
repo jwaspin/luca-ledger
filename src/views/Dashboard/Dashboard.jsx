be8cb4f3361dfb641e496a7b8bead3e54414fe95
Johnathan Aspinwall <jwaspin@gmail.com>
2025-10-09 14:43:52 -0600

    Bump version to 1.8.5-final and update Dashboard to include data saving instructions and a Save All button

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { Box, Link, Typography } from '@mui/material';
+import SaveAllButton from '@/views/Accounts/SaveAllButton';
 
 export default function Dashboard() {
   return (
@@ -34,6 +35,20 @@ export default function Dashboard() {
       >
         https://lucaledger.app
       </Link>
+
+      <Typography
+        variant='body1'
+        sx={{ mt: 3, textAlign: 'center', maxWidth: '600px' }}
+      >
+        This page will remain active for a short period to allow users to save
+        their data and transition to the new site. It is recommended that you
+        use the button below to save your data and load it into the new site.
+        Thank you for your understanding and continued support!
+      </Typography>
+
+      <Box sx={{ mt: 2 }}>
+        <SaveAllButton />
+      </Box>
     </Box>
   );
 }
